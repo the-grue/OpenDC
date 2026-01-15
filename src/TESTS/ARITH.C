de9686d3e7cbda87fdf96bded26fac4bbb4eb211
Peter <peter@email>
2026-01-14 21:43:08 -0800

    Tests of mixed-sign arithmetic

--- src/TESTS/ARITH.C
+++ src/TESTS/ARITH.C
@@ -33,11 +33,11 @@ int f2() {
 	check(k, 0, "add failed: u8 + u8 -> int");
 
 	i = 5; j = 3; k = i - j;
-	check(k, 2, "subtract failed: int - u8 -> u8");
+	check(k, 2, "subtract failed: u8 - u8 -> int");
 
 	// Desmet-ism, not standard C89
 	i = 0; j = 3; k = i - j;
-	check(k, 253, "subtract failed: int - u8 -> u8");
+	check(k, 253, "subtract failed: u8 - u8 -> int");
 
 	return failures;
 }
@@ -74,16 +74,43 @@ int f4() {
 	check(k, -128, "add failed: i8 + i8 -> int");
 
 	i = -1; j = -3; k = i - j;
-	check(k, 2, "subtract failed: int - i8 -> i8");
+	check(k, 2, "subtract failed: i8 - i8 -> int");
 
 	// Desmet-ism, not standard C89
 	i = -128; j = 3; k = i - j;
-	check(k, 125, "subtract failed: int - i8 -> i8");
+	check(k, 125, "subtract failed: i8 - i8 -> int");
 
 	return failures;
 }
 
 int f5() {
+	unsigned char i;
+	signed char j;
+	int k;
+	int failures = 0;
+
+	// Desmet-ism: u8-i8 math yields a u8 result that is then
+	// widened to int.
+	i = 2; j = -3; k = i + j;
+	check(k, 255, "add failed: u8 + i8 -> int");
+	printf("%d\n", k);
+
+	i = 125; j = 3; k = i + j;
+	check(k, 128, "add failed: u8 + i8 -> int");
+	printf("%d\n", k);
+
+	i = 1; j = -3; k = i - j;
+	check(k, 4, "subtract failed: u8 - i8 -> int");
+	printf("%d\n", k);
+
+	i = -128; j = 3; k = i - j;
+	check(k, 125, "subtract failed: u8 - i8 -> int");
+	printf("%d\n", k);
+
+	return failures;
+}
+
+int f6() {
 	signed char i, j;
 	long k;
 	int failures = 0;
@@ -96,11 +123,11 @@ int f5() {
 	check(k, -128, "add failed: i8 + i8 -> long");
 
 	i = -1; j = -3; k = i - j;
-	check(k, 2, "subtract failed: long - i8 -> i8");
+	check(k, 2, "subtract failed: i8 - i8 -> long");
 
 	// Desmet-ism, not standard C89
 	i = -128; j = 3; k = i - j;
-	check(k, 125, "subtract failed: long - i8 -> i8");
+	check(k, 125, "subtract failed: i8 - i8 -> long");
 
 	return failures;
 }
@@ -113,6 +140,7 @@ int main()
 	failures += f3();
 	failures += f4();
 	failures += f5();
+	failures += f6();
 	printf("tests finished, %d failures.\n", failures);
 	return 0;
 }
